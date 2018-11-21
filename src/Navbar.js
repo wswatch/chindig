2a93663e39c4a81fed6da7cb07127b3514ab4e86
nconrad <nconrad@anl.gov>
2018-11-20 23:39:22 -0600

    some responsiveness

--- src/Navbar.js
+++ src/Navbar.js
@@ -8,9 +8,12 @@ import IconButton from '@material-ui/core/IconButton';
 import Button from '@material-ui/core/Button';
 import GearIcon from '@material-ui/icons/Settings';
 import SearchIcon from '@material-ui/icons/Search';
+import CloseIcon from '@material-ui/icons/Close';
 import { fade } from '@material-ui/core/styles/colorManipulator';
 import MenuIcon from '@material-ui/icons/Menu';
 import InputBase from '@material-ui/core/InputBase';
+
+import Hidden from '@material-ui/core/Hidden';
 
 const styles = theme => ({
   root: {
@@ -21,7 +24,6 @@ const styles = theme => ({
   },
   menuButton: {
     marginLeft: -12,
-    marginRight: 20,
   },
   title: {
     display: 'none',
@@ -77,51 +79,92 @@ class Navbar extends React.Component {
   constructor(props) {
     super(props);
 
-    console.log('props', props)
+    this.state = {
+      showSearch: false
+    };
   }
 
-  state = {
-    name: 'Cat in the Hat',
-    age: '',
-    multiline: 'Controlled',
-    currency: 'EUR',
-  };
-
   handleChange = event => {
-
     let query = event.target.value
     this.props.onSearch(query)
   };
 
+
+  toggleSearch = () => {
+    this.setState(oldState => ({
+      showSearch: !oldState.showSearch
+    }));
+  }
+
+
   render() {
     const { classes } = this.props;
-    const onSearch = this.props;
+    const { showSearch } = this.state;
 
     return (
       <div className={classes.root}>
         <AppBar position="static">
           <Toolbar>
-            <IconButton className={classes.menuButton} color="inherit" aria-label="Open drawer">
-              <MenuIcon />
+            <IconButton className={classes.menuButton} color="inherit" aria-label="Settings Menu">
+              <GearIcon />
             </IconButton>
-            <Typography className={classes.title} variant="h6" color="inherit" noWrap>
-              Material-UI
-            </Typography>
-            <div className={classes.grow} />
 
-            <div className={classes.search}>
-              <div className={classes.searchIcon}>
-                <SearchIcon />
+
+            {!showSearch &&
+                <Typography variant="h6" color="inherit" className={classes.grow}>
+                  <div className="logo">Chindig</div>
+                </Typography>}
+
+
+            {/* desktop */}
+            <Hidden smDown>
+
+              <div className={classes.search}>
+                <div className={classes.searchIcon}>
+                  <SearchIcon />
+                </div>
+                <InputBase
+                  onChange={this.handleChange}
+                  placeholder="Search…"
+                  classes={{
+                    root: classes.inputRoot,
+                    input: classes.inputInput,
+                  }}
+                />
               </div>
-              <InputBase
-                onChange={this.handleChange}
-                placeholder="Search…"
-                classes={{
-                  root: classes.inputRoot,
-                  input: classes.inputInput,
-                }}
-              />
-            </div>
+            </Hidden>
+
+            {/* mobile */}
+            {showSearch &&
+              <Hidden mdUp>
+                <div className={classes.search}>
+                <div className={classes.searchIcon}>
+                  <SearchIcon />
+                </div>
+                <InputBase
+                  onChange={this.handleChange}
+                  placeholder="Search…"
+                  classes={{
+                    root: classes.inputRoot,
+                    input: classes.inputInput,
+                  }}
+                />
+              </div>
+            </Hidden>}
+
+            {!showSearch &&
+            <Hidden mdUp>
+              <Button className={classes.menuButton} color="inherit" aria-label="open search button" onClick={this.toggleSearch}>
+                <SearchIcon />
+              </Button>
+            </Hidden>}
+
+            {showSearch &&
+              <Button className={classes.menuButton} color="inherit" aria-label="close search button" onClick={this.toggleSearch}>
+                <CloseIcon />
+              </Button>
+              }
+
           </Toolbar>
         </AppBar>
       </div>
